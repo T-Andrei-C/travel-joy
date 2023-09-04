1e2dbe97ab30d0301c231621ec8e8505c092ad72
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-09-04 13:55:29 +0300

    setup the App run clas

---
+++ src/main/java/com/codecool/TouristsParadiseApplication.java
@@ -0,0 +1,11 @@
+package com.codecool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TouristsParadiseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TouristsParadiseApplication.class, args);
+    }
+}
