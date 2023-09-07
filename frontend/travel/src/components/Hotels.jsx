a0cd2f4f11be3ced26291ece9aa6c0bc3ccf8e75
Andrew <toroaba_andrei@yahoo.com>
2023-09-07 10:00:17 +0300

    Used fetch on our db

---
+++ frontend/travel/src/components/Hotels.jsx
@@ -0,0 +1,15 @@
+import {useEffect, useState} from "react";
+
+const fetchHotels = async () => {
+    return fetch("http://localhost:8080/travel/api/accommodations").then(res => res.json());
+}
+export default function Hotels () {
+    const [hotels, setHotels] = useState([]);
+
+    useEffect(() => {
+        fetchHotels()
+            .then((accommodations) => {
+                setHotels(accommodations);
+            })
+    }, []);
+}
