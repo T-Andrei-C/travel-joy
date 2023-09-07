1170b4e3e557a5c9cf386678389cc2073ffdd18c
Andrew <toroaba_andrei@yahoo.com>
2023-09-07 21:08:02 +0300

    Made some css for input and submit button

--- frontend/travel/src/components/Home.jsx
+++ frontend/travel/src/components/Home.jsx
@@ -26,10 +26,27 @@ const Home = () => {
                 <big id="romania"> Romania</big>
             </h1>
         </div>
-            <div className="input-group flex-nowrap" id="searchDiv">
-                <span className="input-group-text" id="addon-wrapping"><FaMagnifyingGlass/></span>
-                <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping"/>
+            <div id="home-elements">
+            <div className="input-group mb-3 input-group-sm mb-3" id="input-and-drop">
+                <span className="input-group-text text-bg-secondary" id="addon-wrapping"><FaMagnifyingGlass/></span>
+                <input type="text" className="form-control" aria-label="Text input with dropdown button"/>
+                    <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</button>
+                    <ul className="dropdown-menu dropdown-menu-end ">
+                        <li><a className="dropdown-item" href="#">Action</a></li>
+                        <li><a className="dropdown-item" href="#">Another action</a></li>
+                        <li><a className="dropdown-item" href="#">Something else here</a></li>
+                        <li><hr className="dropdown-divider"/></li>
+                        <li><a className="dropdown-item" href="#">Separated link</a></li>
+                    </ul>
             </div>
+            <div>
+                <button id="home-button" type="button" className="btn btn-outline-success">Success</button>
+            </div>
+            </div>
+            {/*<div className="input-group flex-nowrap" id="searchDiv">*/}
+            {/*    <span className="input-group-text" id="addon-wrapping"><FaMagnifyingGlass/></span>*/}
+            {/*    <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping"/>*/}
+            {/*</div>*/}
         </>
     );
 }
