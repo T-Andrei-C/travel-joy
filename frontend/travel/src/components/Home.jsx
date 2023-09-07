071aa81d695707dc4967748a75c7ed9e0b4d6822
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-09-07 15:36:37 +0300

    work at the css of the home page

--- frontend/travel/src/components/Home.jsx
+++ frontend/travel/src/components/Home.jsx
@@ -1,5 +1,7 @@
 import {useEffect, useState} from "react";
 import {getCity} from "../service/CRUDCity";
+import homeImage from "./img/homeImage.jpg";
+import { FaMagnifyingGlass } from "react-icons/fa6"
 
 
 const Home = () => {
@@ -13,11 +15,22 @@ const Home = () => {
             })
     }, []);
     return(
-        <div>
-            {cities.map(c => (
-                <h1>{c.name}</h1>
-            ))}
+        <>
+        <div className="blur">
+            <img src={homeImage} className="img-fluid"/>
         </div>
+
+        <div className="message">
+            <h1 className="text-center text-white" id="description-txt">
+                Explore the unknown place's of
+                <big id="romania"> Romania</big>
+            </h1>
+        </div>
+            <div className="input-group flex-nowrap" id="searchDiv">
+                <span className="input-group-text" id="addon-wrapping"><FaMagnifyingGlass/></span>
+                <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping"/>
+            </div>
+        </>
     );
 }
 
