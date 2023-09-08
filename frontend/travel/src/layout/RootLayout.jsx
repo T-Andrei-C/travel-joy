dab28e74e48f44f4bd66b30b221131f97ccf467d
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-09-08 13:17:13 +0300

    Add some styles for nav bar

--- frontend/travel/src/layout/RootLayout.jsx
+++ frontend/travel/src/layout/RootLayout.jsx
@@ -11,7 +11,19 @@ const RootLayout = () => {
                     </NavLink>
                     <div>
                         <NavLink to="/">
-                            <a id="nav-links" className="nav-link">Features</a>
+                            <a id="nav-links" className="nav-link">Accommodations</a>
+                        </NavLink>
+                        <NavLink to="/">
+                            <a id="nav-links" className="nav-link">Packages</a>
+                        </NavLink>
+                        <NavLink to="/">
+                            <a id="nav-links" className="nav-link">About us</a>
+                        </NavLink>
+                        <NavLink to="/">
+                            <a id="nav-links" className="nav-link">Log in</a>
+                        </NavLink>
+                        <NavLink to="/">
+                            <a id="nav-links" className="nav-link">Sign up</a>
                         </NavLink>
                     </div>
                 </nav>
