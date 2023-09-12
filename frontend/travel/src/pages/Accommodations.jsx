6cde1d0f5bc3e3e0a92ab73bcb864d215aef7247
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-09-12 11:31:54 +0300

    Add cards on accomodation page

--- frontend/travel/src/pages/Accommodations.jsx
+++ frontend/travel/src/pages/Accommodations.jsx
@@ -1,5 +1,6 @@
 import {useState,useEffect} from "react";
 import {getAccommodations} from "../service/CRUDAccommodations";
+import testImage from "../components/img/homeImage.jpg";
 
 const Accommodations = () =>{
     const[accommodations,setAccommodations] = useState([]);
@@ -12,7 +13,27 @@ const Accommodations = () =>{
     }, []);
 
     return(
-        <div>{accommodations.length !== 0 && accommodations[0].name}</div>
+        <div className="cards-container ">
+            {accommodations.length !== 0 && accommodations.map(a =>
+                <div key={a.id} className=" card col-6 mb-3 border-success m-5" style={{maxWidth : "650px"}}>
+                    <div className="row no-gutters">
+                        <div className="col-md-4">
+                            <img src={testImage} className="card-img" style={{height:"11em"}} />
+                            <div className="card-img-overlay">
+                                <h6 className="card-title">{a.name}</h6>
+                            </div>
+                        </div>
+                        <div className="col-md-8">
+                            <div className="card-body">
+                                <h5 className="card-title">{a.city.name}</h5>
+                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </div>
     );
 }
 
