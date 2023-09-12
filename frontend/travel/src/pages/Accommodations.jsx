cdb585e858671b6efea7d50469d6ee117d58fa0b
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-09-12 12:02:34 +0300

    add some design for accommodations card

--- frontend/travel/src/pages/Accommodations.jsx
+++ frontend/travel/src/pages/Accommodations.jsx
@@ -13,21 +13,20 @@ const Accommodations = () =>{
     }, []);
 
     return(
-        <div className="cards-container ">
+        <div className="d-flex align-items-center justify-content-center">
             {accommodations.length !== 0 && accommodations.map(a =>
-                <div key={a.id} className=" card col-6 mb-3 border-success m-5" style={{maxWidth : "650px"}}>
+                <div key={a.id} className="card col-12  border-success m-5" style={{maxWidth : "850px"}}>
                     <div className="row no-gutters">
                         <div className="col-md-4">
-                            <img src={testImage} className="card-img" style={{height:"11em"}} />
-                            <div className="card-img-overlay">
-                                <h6 className="card-title">{a.name}</h6>
-                            </div>
+                            <img src={testImage} className="card-img"  />
                         </div>
                         <div className="col-md-8">
                             <div className="card-body">
-                                <h5 className="card-title">{a.city.name}</h5>
+                                <h3 className="card-title text-success">{a.name}</h3>
+                                <h6 className="card-title text-success">{a.city.name}</h6>
+                                <hr className="border-success"/>
                                 <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+                                <a href="#" className="btn btn-success float-md-end mb-3" >View Details</a>
                             </div>
                         </div>
                     </div>
