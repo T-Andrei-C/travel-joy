f646bbbe244dfc28f34fb4be9ebb6c3b00970518
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-11-08 16:02:06 +0200

    a part of about us page

--- frontend/travel/src/pages/Home.jsx
+++ frontend/travel/src/pages/Home.jsx
@@ -5,9 +5,9 @@ import Input from "../components/home-page/Input"
 const Home = () => {
     return(
         <div className="text-bg-dark" id="blur">
-            <img src={homeImage} className="card-img img-fluid position-absolute object-fit-cover z-n1 w-100 h-100" alt="..."/>
-            <div className="card-img-overlay">
-                <div className="message">
+            <img src="https://cdn.discordapp.com/attachments/1151791153004413009/1171746366070997054/sokol-eugeniu-LD5-9Qr6uqc-unsplash.jpg?ex=655dccd7&is=654b57d7&hm=ced99cd99458e5da72c756268229734472fe99d7f52ebb434689952018e80d6f&" className="card-img img-fluid position-absolute object-fit-cover z-n1 w-100 h-100" alt="..."/>
+            <div className="card-img-overlay mt-5">
+                <div className="message mt-2">
                     <h1 className="text-center text-white" id="description-txt">
                         Explore the unknown places of
                         <big id="romania"> Romania</big>
