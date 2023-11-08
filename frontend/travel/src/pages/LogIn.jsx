d4a44618a066a592c036e3b9306f9bbaf4713b15
Andrew <toroaba_andrei@yahoo.com>
2023-11-08 16:03:49 +0200

    minor changes

--- frontend/travel/src/pages/LogIn.jsx
+++ frontend/travel/src/pages/LogIn.jsx
@@ -42,22 +42,22 @@ function LogIn() {
     }
 
     return (
-        <form onSubmit={onSave} className="d-flex justify-content-center " style={{marginTop: "5em"}}>
+        <form onSubmit={onSave} className="d-flex justify-content-center" style={{marginTop: "5em"}}>
             <div className="col-10 col-md-8 col-lg-6 col-xl-4 card border-success rounded-4">
                 <div className="card-body p-lg-5 p-xl-5 p-md-5 text-center">
                     <h3 className="mb-5">Log in</h3>
 
-                    <div className="form-floating  mb-4">
+                    <div className="form-floating mb-4">
                         <label className="pt-1 text-success fw-bold" style={{fontSize: "0.75em"}}
                                htmlFor="floatingInputValue">Email</label>
                         <input type="email" name="email" className="form-control form-control-lg border-success"
-                               required={true}/>
+                               required={true} style={{fontSize: "1.1em"}}/>
                     </div>
 
                     <div className="form-floating mb-4">
                         <label className="pt-1 text-success fw-bold" style={{fontSize: "0.75em"}}
                                htmlFor="floatingInputValue">Password</label>
-                        <input type="password" name="password" className="form-control form-control-lg border-success" required={true}/>
+                        <input type="password" name="password" className="form-control form-control-lg border-success" required={true} style={{fontSize: "1.1em"}}/>
                     </div>
                     <p className="text-danger" hidden={error === ""}>Email or password are invalid !</p>
                     <button className="btn btn-success btn-lg btn-block " type="submit">LogIn</button>
