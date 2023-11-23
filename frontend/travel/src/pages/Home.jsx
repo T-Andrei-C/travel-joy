f89f9fecf85ff1f8d9df458e5f1e214228cc442c
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2023-11-23 11:11:12 +0200

    comment fade efect for carousel

--- frontend/travel/src/pages/Home.jsx
+++ frontend/travel/src/pages/Home.jsx
@@ -15,19 +15,19 @@ const Home = () => {
         for (let i = 0; i < carouselImages?.length; i++) {
             if (i === imageNumber){
                 carouselImages[i].classList.add("active");
-                carouselImages[i].classList.add("carousel-item-next");
-                carouselImages[i].classList.add("carousel-item-start");
-                setTimeout(() => {
-                    carouselImages[i].classList.remove("carousel-item-next");
-                    carouselImages[i].classList.remove("carousel-item-start");
-
-                },2000);
+                // carouselImages[i].classList.add("carousel-item-next");
+                // carouselImages[i].classList.add("carousel-item-start");
+                // setTimeout(() => {
+                //     carouselImages[i].classList.remove("carousel-item-next");
+                //     carouselImages[i].classList.remove("carousel-item-start");
+                //
+                // },2000);
             } else {
                 carouselImages[i].classList.remove("active");
-                carouselImages[i].classList.add("carousel-item-start");
-                setTimeout(() => {
-                    carouselImages[i].classList.remove("carousel-item-start");
-                },2000);
+                // carouselImages[i].classList.add("carousel-item-start");
+                // setTimeout(() => {
+                //     carouselImages[i].classList.remove("carousel-item-start");
+                // },2000);
 
 
             }
