0dd55981e8f7af87225a6f01f3cf72013d3e7b78
MateiMadalina <116349352+MateiMadalina@users.noreply.github.com>
2024-01-15 10:40:34 +0200

    change fetch with new syntax

--- frontend/travel/src/service/CRUDAccommodations.js
+++ frontend/travel/src/service/CRUDAccommodations.js
@@ -1,13 +1,16 @@
 import {API_URL} from "./API";
 
 export const getAccommodationsPerPage = async (itemsPerPage, numberOfPage) => {
-    return fetch(API_URL + "accommodations/" + itemsPerPage + "/" + numberOfPage).then(res => res.json());
+    const request = await  fetch(`${API_URL}accommodations/${itemsPerPage}/${numberOfPage}`);
+    return await request.json();
 }
 
 export const getAccommodationsByCityName = async (destination, itemsPerPage, numberOfPage) => {
-    return fetch(API_URL + "accommodations/" + destination + "/" + itemsPerPage + "/" + numberOfPage).then(res => res.json());
+    const request = await fetch(`${API_URL}accommodations/${destination}/${itemsPerPage}/${numberOfPage}`);
+    return await request.json();
 }
 
 export const getAccommodationsSearch = async (destination, itemsPerPage, numberOfPage, checkIn, checkOut, numberOfPersons) => {
-    return fetch(`${API_URL}accommodations/${destination}/${checkIn}/${checkOut}/${numberOfPersons}/${itemsPerPage}/${numberOfPage}`).then(res => res.json());
+    const request = await fetch(`${API_URL}accommodations/${destination}/${checkIn}/${checkOut}/${numberOfPersons}/${itemsPerPage}/${numberOfPage}`);
+    return await request.json();
 }
--- frontend/travel/src/service/CRUDTravelPackages.js
+++ frontend/travel/src/service/CRUDTravelPackages.js
@@ -1,13 +1,16 @@
 import {API_URL} from "./API";
 
 export const getTravelPackagesPerPage = async (itemsPerPage, numberOfPage) => {
-    return fetch(`${API_URL}packages/${itemsPerPage}/${numberOfPage}`).then(res => res.json());
+    const request = await fetch(`${API_URL}packages/${itemsPerPage}/${numberOfPage}`);
+    return await request.json();
 }
 
 export const getTravelPackagesByCity= async (destination , itemsPerPage, numberOfPage) => {
-    return fetch(API_URL + "packages/" + destination + "/" + itemsPerPage + "/" + numberOfPage ).then(res => res.json());
+    const request = await fetch(`${API_URL}packages/${destination}/${itemsPerPage}/${numberOfPage}`);
+    return await request.json();
 }
 
 export const getTravelPackagesSearch = async (destination, checkIn, checkOut, numberOfPersons, itemsPerPage, numberOfPage) => {
-    return fetch(`${API_URL}packages/${destination}/${checkIn}/${checkOut}/${numberOfPersons}/${itemsPerPage}/${numberOfPage}`).then(res => res.json());
+   const request = await fetch(`${API_URL}packages/${destination}/${checkIn}/${checkOut}/${numberOfPersons}/${itemsPerPage}/${numberOfPage}`);
+   return await request.json();
 }
